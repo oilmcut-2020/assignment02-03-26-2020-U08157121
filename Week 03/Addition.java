ffcb878e6ae3805ea4b332c08c48e1c6e6ad7691
U08157121 <59036775+U08157121@users.noreply.github.com>
2020-03-30 20:37:49 +0800

    Add files via upload

--- Week 03/Addition.java
+++ Week 03/Addition.java
@@ -1,28 +1,30 @@
-
-// Addition program that displays the sum of two numbers.
-import java.util.Scanner; // program uses class Scanner
-
+package homework;
+import java.util.Scanner;
 public class Addition 
 {
    // main method begins execution of Java application
-   public static void main(String[] args)
-   {
-      // create a Scanner to obtain input from the command window
+   public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
+      int number1;
+      int number2; 
+      int mode;
+      int sum=0; 
+      System.out.print("Enter first integer:");  
+      number1 = input.nextInt(); 
+      System.out.print("Enter second integer:");  
+      number2 = input.nextInt(); 
+      System.out.print("select mode:");  
+      mode=input.nextInt();
+      if (mode==1)
+    	   sum = number1 + number2;
+      else if (mode==2)
+    	   sum = number1 - number2;
+      else if (mode==3)
+    	   sum = number1 * number2;
+      else if (mode==4)
+    	   sum = number1 / number2;
+      
+      System.out.printf("Sum is %d%n", sum); 
 
-      int number1; // first number to add
-      int number2; // second number to add
-      int sum; // sum of number1 and number2
-
-      System.out.print("Enter first integer: "); // prompt 
-      number1 = input.nextInt(); // read first number from user
-
-      System.out.print("Enter second integer: "); // prompt 
-      number2 = input.nextInt(); // read second number from user
-
-      sum = number1 + number2; // add numbers, then store total in sum
-
-      System.out.printf("Sum is %d%n", sum); // display sum
-
-   } // end method main
-} // end class Addition
+   }
+}
